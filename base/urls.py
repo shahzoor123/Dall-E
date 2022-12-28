a28ceadb37e35d2024aa86c5035509e652372601
shahzoorkhan <lamborghinia693@gmail.com>
2022-12-28 20:14:09 +0500

    first commit

---
+++ base/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    
+    path('tourist-guide-form/', views.tourist, name = 'tourist-guide-form'),
+   
+
+]                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
