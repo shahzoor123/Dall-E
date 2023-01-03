47a5cef9913a8a587bb8b883a9d84854d06f64dc
shahzoorkhan <lamborghinia693@gmail.com>
2023-01-04 03:34:24 +0500

    integrated time on template

--- base/views.py
+++ base/views.py
@@ -2,4 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    import time
+    date = time.gmtime()
+    year = date.tm_year
+    month = date.tm_mon
+    day = date.tm_mday
+    hour = date.tm_hour
+    minute = date.tm_min
+    second = date.tm_sec
+
+    context = {
+        'time' : time,
+        'year' : year,
+        'month' : month,
+        'day' : day,
+        'hour' : hour,
+        'min' : minute,
+        'second' : second,
+    }
+    
+    return render(request, 'index.html', context)
